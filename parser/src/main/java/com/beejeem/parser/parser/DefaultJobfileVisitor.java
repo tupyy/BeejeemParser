e38b36363c5fd8e2a8dca0e94c620aea65a35c78
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-06-06 15:54:09 +0200

    fix visitor

--- parser/src/main/java/com/beejeem/parser/parser/DefaultJobfileVisitor.java
+++ parser/src/main/java/com/beejeem/parser/parser/DefaultJobfileVisitor.java
@@ -2,15 +2,12 @@ package com.beejeem.parser.parser;
 
 import com.beejeem.parser.domain.Program;
 import com.beejeem.antrl4.visitor.JobfileBaseVisitor;
+import com.beejeem.antrl4.visitor.JobfileParser;
 
-public class DefaultJobfileVisitor implements Parser {
+public class DefaultJobfileVisitor extends JobfileBaseVisitor<Program> {
 
     @Override
-    public Program parse(String code) {
+    public Program visitProgram(JobfileParser.ProgramContext ctx) {
         return null;
-    }
-
-    private static class ProgramVisitor extends JobfileBaseVisitor<Program> {
-
     }
 }
