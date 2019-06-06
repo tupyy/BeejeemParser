c28b0a88cbe82260e64524948bc147a6d9b4df71
cosmin <cosmin@home.net>
2019-06-06 22:12:50 +0200

    Fix float variable

--- parser/src/main/java/com/beejeem/parser/domain/variables/FloatVariable.java
+++ parser/src/main/java/com/beejeem/parser/domain/variables/FloatVariable.java
@@ -1,12 +1,12 @@
 package com.beejeem.parser.domain.variables;
 
-public class FloatVariable extends AbstractVariable<Boolean> {
+public class FloatVariable extends AbstractVariable<Float> {
 
     public FloatVariable(String name) {
         super(name);
     }
 
-    public FloatVariable(String name, Boolean value) {
+    public FloatVariable(String name, Float value) {
         super(name, value);
     }
 }
