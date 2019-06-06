fa750d61674f12145d621b4c9d6dce5451c5d245
cosmin <cosmin@home.net>
2019-06-06 22:50:06 +0200

    patch list variable to be a wrapper of java.lang.list
    It is just to implement Variable interface

--- parser/src/main/java/com/beejeem/parser/domain/variables/ListVariable.java
+++ parser/src/main/java/com/beejeem/parser/domain/variables/ListVariable.java
@@ -1,12 +1,30 @@
 package com.beejeem.parser.domain.variables;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ListVariable<T> extends AbstractVariable<T> {
+
+    private List<T> values = new ArrayList<>();
 
     public ListVariable(String name) {
         super(name);
     }
 
-    public ListVariable(String name, T value) {
-        super(name, value);
+    public void add(T... values) {
+        this.values.addAll(Arrays.asList(values));
+    }
+
+    public void add(List<T> values) {
+        this.values.addAll(values);
+    }
+
+    public T getValue() {
+        throw new UnsupportedOperationException("Not supported for list variables");
+    }
+
+    public void setValues(T value) {
+        throw new UnsupportedOperationException("Not supported for list variables");
     }
 }
