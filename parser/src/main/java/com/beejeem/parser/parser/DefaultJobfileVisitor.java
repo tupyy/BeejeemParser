6bf27bcbfbbd613216eae65d03e7454ea103cc3d
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-06-11 08:57:21 +0200

    parse line contexts

--- parser/src/main/java/com/beejeem/parser/parser/DefaultJobfileVisitor.java
+++ parser/src/main/java/com/beejeem/parser/parser/DefaultJobfileVisitor.java
@@ -1,23 +1,26 @@
 package com.beejeem.parser.parser;
 
-import com.beejeem.parser.domain.Program;
 import com.beejeem.antrl4.JobfileBaseVisitor;
 import com.beejeem.antrl4.JobfileParser;
-import com.beejeem.parser.domain.variables.Variable;
-import com.beejeem.parser.parser.visitors.VarAssigmentVisitor;
+import com.beejeem.parser.domain.Program;
+import com.beejeem.parser.domain.Statement;
+import com.beejeem.parser.parser.visitors.LineVisitor;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class DefaultJobfileVisitor extends JobfileBaseVisitor<Program> {
 
-    @Override
-    public Program visitProgram(JobfileParser.ProgramContext ctx) {
-        visitChildren(ctx);
-        return null;
-    }
+    private Program program = new Program();
 
     @Override
-    public Program visitVarstmt(JobfileParser.VarstmtContext ctx) {
-        VarAssigmentVisitor variableVarAssigmentVisitor = new VarAssigmentVisitor();
-        Variable  variable = ctx.varassignment().accept(variableVarAssigmentVisitor);
-        return null;
+    public Program visitProgram(JobfileParser.ProgramContext ctx) {
+        List<Statement> statements = ctx.children.stream()
+                                                 .map(lineCtx -> lineCtx.accept(new LineVisitor()))
+                                                 .collect(Collectors.toList());
+        for (Statement statement: statements) {
+            program.add(statement);
+        }
+        return program;
     }
 }
