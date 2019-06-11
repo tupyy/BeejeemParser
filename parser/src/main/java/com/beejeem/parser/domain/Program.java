daedf15cb2be13ea0044a3f596e30148edec4567
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-06-11 08:57:38 +0200

    implement add statement method

--- parser/src/main/java/com/beejeem/parser/domain/Program.java
+++ parser/src/main/java/com/beejeem/parser/domain/Program.java
@@ -1,4 +1,20 @@
 package com.beejeem.parser.domain;
 
+
+import com.beejeem.parser.domain.variables.Variable;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program {
+
+    List<Variable> variables = new ArrayList<>();
+
+    public Program() {}
+
+    public void add(Statement statement) {
+        if (statement instanceof Variable) {
+            variables.add((Variable) statement);
+        }
+    }
 }
