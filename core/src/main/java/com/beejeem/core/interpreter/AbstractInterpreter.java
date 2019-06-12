768819dc373e39f0472ad02e417ea4e39f68eb19
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-06-12 15:45:30 +0200

    add base interface and abstract class for interpreter

---
+++ core/src/main/java/com/beejeem/core/interpreter/AbstractInterpreter.java
@@ -0,0 +1,34 @@
+package com.beejeem.core.interpreter;
+
+import com.beejeem.core.executable.CommandExecutable;
+import com.beejeem.parser.domain.commands.Command;
+import com.beejeem.parser.domain.variables.Variable;
+
+import java.util.List;
+
+public abstract class AbstractInterpreter implements CommandInterpreter<CommandExecutable> {
+
+    public enum OS_TYPE {
+        LINUX,
+        WINDOWS,
+        OSX,
+        UNKNWON
+    }
+
+    private static String OS = System.getProperty("os.name").toLowerCase();
+
+    @Override
+    public abstract CommandExecutable interpret(Command command, List<Variable> variables);
+
+    public OS_TYPE get_os_type() {
+        if (OS.contains("nix") || OS.contains("nux")) {
+            return OS_TYPE.LINUX;
+        } else if (OS.contains("win")) {
+            return OS_TYPE.WINDOWS;
+        } else if (OS.contains("mac")) {
+            return OS_TYPE.OSX;
+        }
+        return OS_TYPE.UNKNWON;
+    }
+
+}
---
+++ core/src/main/java/com/beejeem/core/interpreter/CommandInterpreter.java
@@ -0,0 +1,22 @@
+package com.beejeem.core.interpreter;
+
+import com.beejeem.parser.domain.commands.Command;
+import com.beejeem.parser.domain.variables.Variable;
+
+import java.util.List;
+
+/**
+ * Interface for command interpreter.
+ * A command interpreter is a class which will transform the command object into an executable class.
+ *
+ * For local command types, the executable class is a class which will execute a local process based on the type of the
+ * command (e.g for {@link com.beejeem.parser.domain.commands.Command.CommandType} Copy which will be executed on linux
+ * a "cp" command will be executed.
+ *
+ * For remote command types, a ssh session must be created first and than a remote command will be executed
+ * @param <T>
+ */
+public interface CommandInterpreter<T> {
+
+    public T interpret(Command command, List<Variable> variables);
+}
