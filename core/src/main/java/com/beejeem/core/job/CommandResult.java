35a9f17a9c4c56e8f865d6ba4819f4bfbee36f5a
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-06-12 14:56:24 +0200

    getter for command id

--- core/src/main/java/com/beejeem/core/job/CommandResult.java
+++ core/src/main/java/com/beejeem/core/job/CommandResult.java
@@ -1,5 +1,7 @@
 package com.beejeem.core.job;
 
+import java.util.UUID;
+
 public interface CommandResult {
 
     public enum CommandResultStatus {
@@ -7,7 +9,17 @@ public interface CommandResult {
         ERROR
     }
 
+    /**
+     * Get command exit status
+     * @return status
+     */
     public CommandResultStatus getResultStatus();
+
+    /**
+     * Get the id of the command
+     * @return id of the command
+     */
+    public UUID getCommandID();
 
 
 }
