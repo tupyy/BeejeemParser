d2736acdeefcb01e64ebd45ea4663c2086aa9ea4
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-06-12 10:57:36 +0200

    implement process command method
    if the result is ok, pass to the next command. go to error state
    otherwise

--- core/src/main/java/com/beejeem/core/job/DefaultJob.java
+++ core/src/main/java/com/beejeem/core/job/DefaultJob.java
@@ -2,6 +2,7 @@ package com.beejeem.core.job;
 
 import com.beejeem.parser.domain.Program;
 import com.beejeem.parser.domain.commands.Command;
+import com.github.oxo42.stateless4j.delegates.Action2;
 
 import java.util.UUID;
 
@@ -13,8 +14,8 @@ public final class DefaultJob extends JobStateMachine implements Job {
     private final String name;
     private final Program program;
 
-    public DefaultJob(String name, Program program) {
-        super(program, new JobStateChangeAction());
+    public DefaultJob(String name, Program program, Action2 commandAction) {
+        super(program, new JobStateChangeAction(), commandAction);
 
         checkNotNull(name);
 
@@ -60,7 +61,18 @@ public final class DefaultJob extends JobStateMachine implements Job {
     }
 
     @Override
-    public void doCommand(Command command) {
-
+    public void processCommandResult(CommandResult result) {
+        if (result.getResultStatus() == CommandResult.CommandResultStatus.OK) {
+            if (program.hasNext()) {
+                Command nextCommand = program.next();
+                this.getStateMachine().fire(this.getCommandTrigger(),nextCommand,program.getVariables());
+            }
+            else {
+                this.getStateMachine().fire(Trigger.doFinish);
+            }
+        } else {
+            this.getStateMachine().fire(Trigger.doError);
+        }
     }
+
 }
