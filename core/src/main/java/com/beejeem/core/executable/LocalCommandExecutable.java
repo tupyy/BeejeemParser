94b3d198ddfa91dcaa0d4c5217e0daad973633a3
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-06-14 07:47:20 +0200

    run the ProcBuilder and capture the output in a stream. log the error

--- core/src/main/java/com/beejeem/core/executable/LocalCommandExecutable.java
+++ core/src/main/java/com/beejeem/core/executable/LocalCommandExecutable.java
@@ -1,12 +1,19 @@
 package com.beejeem.core.executable;
 
 import com.beejeem.core.result.CommandResult;
+import com.beejeem.core.result.CommandResultImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.buildobjects.process.ExternalProcessFailureException;
 import org.buildobjects.process.ProcBuilder;
 import org.buildobjects.process.ProcResult;
 
+import java.io.ByteArrayOutputStream;
 import java.util.UUID;
 
 public final class LocalCommandExecutable implements CommandExecutable {
+
+    private static Logger logger = LogManager.getLogger(LocalCommandExecutable.class);
 
     private final ProcBuilder procBuilder;
     private final UUID jobID;
@@ -20,7 +27,17 @@ public final class LocalCommandExecutable implements CommandExecutable {
 
     @Override
     public CommandResult execute() {
-        ProcResult result = this.procBuilder.run();
-        return null;
+        CommandResult commandResult = new CommandResultImpl(this.jobID, this.commandID);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try {
+            this.procBuilder.withOutputStream(out).run();
+            commandResult.setCommandResultStatus(CommandResult.CommandResultStatus.OK);
+            logger.debug("Command " + this.commandID.toString() + " output :" + out.toString());
+        }
+        catch (ExternalProcessFailureException ex) {
+            logger.error("Command " + this.commandID.toString() + " failed: " + out.toString());
+            commandResult.setCommandResultStatus(CommandResult.CommandResultStatus.ERROR);
+        }
+        return commandResult;
     }
 }
