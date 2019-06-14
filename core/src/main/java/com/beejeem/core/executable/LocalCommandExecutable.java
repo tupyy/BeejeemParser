86f514483f0d75284187226c5b48efbf06152564
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-06-14 14:46:20 +0200

    add output variables to command if any

--- core/src/main/java/com/beejeem/core/executable/LocalCommandExecutable.java
+++ core/src/main/java/com/beejeem/core/executable/LocalCommandExecutable.java
@@ -2,13 +2,18 @@ package com.beejeem.core.executable;
 
 import com.beejeem.core.result.CommandResult;
 import com.beejeem.core.result.CommandResultImpl;
+import com.beejeem.core.result.JsonExtractor;
+import com.beejeem.parser.domain.variables.Variable;
+import com.beejeem.parser.parser.JsonParser;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.buildobjects.process.ExternalProcessFailureException;
 import org.buildobjects.process.ProcBuilder;
-import org.buildobjects.process.ProcResult;
 
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public final class LocalCommandExecutable implements CommandExecutable {
@@ -31,13 +36,26 @@ public final class LocalCommandExecutable implements CommandExecutable {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         try {
             this.procBuilder.withOutputStream(out).run();
-            commandResult.setCommandResultStatus(CommandResult.CommandResultStatus.OK);
             logger.debug("Command " + this.commandID.toString() + " output :" + out.toString());
+
+            commandResult.setVariables(this.extractOutputVariables(out.toString()));
+            commandResult.setCommandResultStatus(CommandResult.CommandResultStatus.OK);
         }
         catch (ExternalProcessFailureException ex) {
             logger.error("Command " + this.commandID.toString() + " failed: " + ex.getMessage());
             commandResult.setCommandResultStatus(CommandResult.CommandResultStatus.ERROR);
         }
         return commandResult;
+    }
+
+    private List<Variable> extractOutputVariables(String output) {
+        JsonParser jsonParser = new JsonParser();
+        try {
+            return jsonParser.parse(JsonExtractor.extract(output));
+        }
+        catch (ParseCancellationException ex) {
+            logger.error(ex.getMessage());
+            return new ArrayList<>();
+        }
     }
 }
