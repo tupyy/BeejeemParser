ea5d5406b19091bf3f5a071f5a78a5c1fb3f348a
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-06-20 14:46:29 +0200

    add log info when submitting

--- core/src/main/java/com/beejeem/core/executor/DefaultCommandExecutor.java
+++ core/src/main/java/com/beejeem/core/executor/DefaultCommandExecutor.java
@@ -3,6 +3,8 @@ package com.beejeem.core.executor;
 import com.beejeem.core.command.executable.CommandExecutable;
 import com.beejeem.core.command.executable.LocalCommandExecutable;
 import com.beejeem.core.command.result.CommandResult;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
@@ -11,6 +13,8 @@ import java.util.concurrent.ThreadPoolExecutor;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class DefaultCommandExecutor implements CommandExecutor<CommandExecutable> {
+
+    private static Logger logger = LogManager.getLogger(DefaultCommandExecutor.class);
 
     private final CommandResultManager commandResultManager;
     private ThreadPoolExecutor localExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
@@ -25,8 +29,10 @@ public class DefaultCommandExecutor implements CommandExecutor<CommandExecutable
     public void submit(CommandExecutable executable) {
         CompletableFuture<CommandResult> result;
         if (executable instanceof LocalCommandExecutable) {
+             logger.info(String.format("Submitting command <%s> of job <%s> to local executor", executable.getCommandID(), executable.getJobID()));
              result = (CompletableFuture<CommandResult>) localExecutor.submit(executable);
         } else {
+            logger.info(String.format("Submitting command <%s> of job <%s> to remote executor", executable.getCommandID(), executable.getJobID()));
             result = (CompletableFuture<CommandResult>) remoteExecutor.submit(executable);
         }
         result.thenAccept(this.commandResultManager::setResult);
