d90220fd3f71c68a06d6922547d46bb4bc421615
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-06-20 15:19:23 +0200

    empty result manager implementation

---
+++ core/src/main/java/com/beejeem/core/executor/CommandResultManagerImpl.java
@@ -0,0 +1,11 @@
+package com.beejeem.core.executor;
+
+import com.beejeem.core.command.result.CommandResult;
+
+public final class CommandResultManagerImpl implements CommandResultManager<CommandResult> {
+
+    @Override
+    public void setResult(CommandResult commandResult) {
+
+    }
+}
