c4bf394b4c83dd4a786181f32d70577cf7741a06
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-06-25 13:36:13 +0200

    create spring context class

---
+++ core/src/main/java/com/beejeem/core/CoreContext.java
@@ -0,0 +1,9 @@
+package com.beejeem.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.beejeem.core")
+public class CoreContext {
+}
