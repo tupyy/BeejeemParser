6600fdd9a16ec2365485e20ec0b1e5a0bba84286
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2019-07-03 14:40:14 +0200

    add tests for for loops fix some other test data

--- parser/src/test/com/beejeem/parser/test/TestParser.java
+++ parser/src/test/com/beejeem/parser/test/TestParser.java
@@ -26,86 +26,87 @@ public class TestParser {
 
     @Test
     public void testMathOperations() {
-        try {
-            ExecutionContext executionContext = new ExecutionContext();
-            ProgramContext programContext =this.parse(readTestFile("math_op.txt"));
-            final ProgramListener programListener = new ProgramListener(executionContext);
-            programContext.enterRule(programListener);
+        ExecutionContext executionContext = new ExecutionContext();
+        ProgramContext programContext =this.parse(readTestFile("math_op.txt"));
+        final ProgramListener programListener = new ProgramListener(executionContext);
+        programContext.enterRule(programListener);
 
-            StackFrame stackFrame = programListener.getLastStack();
-            Value var = stackFrame.getVariable("c");
-            assertEquals(1f, var.getValue());
-            assertEquals(8f, stackFrame.getVariable("cc").getValue());
-            assertEquals(4f, stackFrame.getVariable("p").getValue());
-            assertEquals(24, stackFrame.getVariable("i1").getValue());
-            assertEquals(false, stackFrame.getVariable("b1").getValue());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        StackFrame stackFrame = programListener.getLastStack();
+        Value var = stackFrame.getVariable("c");
+        assertEquals(1f, var.getValue());
+        assertEquals(8f, stackFrame.getVariable("cc").getValue());
+        assertEquals(4f, stackFrame.getVariable("p").getValue());
+        assertEquals(24, stackFrame.getVariable("i1").getValue());
+        assertEquals(false, stackFrame.getVariable("bb1").getValue());
     }
 
     @Test
     public void testAssignmentErrors() {
         String stringToInt = "int a=\"string\";";
-        try {
-            ExecutionContext executionContext = new ExecutionContext();
-            ProgramContext programContext =this.parse(new ByteArrayInputStream(stringToInt.getBytes()));
-            assertThrows(InvalidOperationException.class, () -> {
-                final ProgramListener programListener = new ProgramListener(executionContext);
-                programContext.enterRule(programListener);
-            });
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        ExecutionContext executionContext = new ExecutionContext();
+        ProgramContext programContext =this.parse(new ByteArrayInputStream(stringToInt.getBytes()));
+        assertThrows(InvalidOperationException.class, () -> {
+            final ProgramListener programListener = new ProgramListener(executionContext);
+            programContext.enterRule(programListener);
+        });
     }
 
     @Test
     public void testAssignmentErrors2() {
         String stringToInt = "bool a=\"string\";";
-        try {
-            ExecutionContext executionContext = new ExecutionContext();
-            ProgramContext programContext =this.parse(new ByteArrayInputStream(stringToInt.getBytes()));
-            assertThrows(InvalidOperationException.class, () -> {
-                final ProgramListener programListener = new ProgramListener(executionContext);
-                programContext.enterRule(programListener);
-            });
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        ExecutionContext executionContext = new ExecutionContext();
+        ProgramContext programContext =this.parse(new ByteArrayInputStream(stringToInt.getBytes()));
+        assertThrows(InvalidOperationException.class, () -> {
+            final ProgramListener programListener = new ProgramListener(executionContext);
+            programContext.enterRule(programListener);
+        });
     }
 
     @Test
     public void testIfs() {
-        try {
-            ExecutionContext executionContext = new ExecutionContext();
-            ProgramContext programContext =this.parse(readTestFile("ifstatements.txt"));
-            final ProgramListener programListener = new ProgramListener(executionContext);
-            programContext.enterRule(programListener);
+        ExecutionContext executionContext = new ExecutionContext();
+        ProgramContext programContext =this.parse(readTestFile("ifstatements.txt"));
+        final ProgramListener programListener = new ProgramListener(executionContext);
+        programContext.enterRule(programListener);
 
-            StackFrame stackFrame = programListener.getLastStack();
-            assertEquals(2, stackFrame.getVariable("c").getValue());
-            assertEquals(1, stackFrame.getVariable("d").getValue());
-            assertEquals(0, stackFrame.getVariable("e").getValue());
-            assertEquals(1, stackFrame.getVariable("f").getValue());
-            assertEquals(1, stackFrame.getVariable("g").getValue());
-            assertEquals(1, stackFrame.getVariable("h").getValue());
-            assertEquals(1, stackFrame.getVariable("i").getValue());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        StackFrame stackFrame = programListener.getLastStack();
+        assertEquals(2, stackFrame.getVariable("c").getValue());
+        assertEquals(1, stackFrame.getVariable("d").getValue());
+        assertEquals(0, stackFrame.getVariable("e").getValue());
+        assertEquals(1, stackFrame.getVariable("f").getValue());
+        assertEquals(1, stackFrame.getVariable("g").getValue());
+        assertEquals(1, stackFrame.getVariable("h").getValue());
+        assertEquals(1, stackFrame.getVariable("i").getValue());
+    }
+
+    @Test
+    public void testFors() {
+        ExecutionContext executionContext = new ExecutionContext();
+        ProgramContext programContext =this.parse(readTestFile("forstatements.txt"));
+        final ProgramListener programListener = new ProgramListener(executionContext);
+        programContext.enterRule(programListener);
+
+        StackFrame stackFrame = programListener.getLastStack();
+        assertEquals(5, stackFrame.getVariable("b").getValue());
+        assertEquals(4, stackFrame.getVariable("c").getValue());
+        assertEquals(450, stackFrame.getVariable("d").getValue());
+        assertEquals(4500, stackFrame.getVariable("f").getValue());
     }
 
     private InputStream readTestFile(String filename) {
         return getClass().getClassLoader().getResourceAsStream(filename);
     }
 
-    private ProgramContext parse(InputStream stream) throws IOException {
-        final Lexer lexer = new bjmLexer(CharStreams.fromStream(stream));
-        final TokenStream tokenStream = new CommonTokenStream(lexer);
-        final bjmParser parser = new bjmParser(tokenStream);
-        return parser.program();
+    private ProgramContext parse(InputStream stream)  {
+        try {
+            final Lexer lexer = new bjmLexer(CharStreams.fromStream(stream));
+            final TokenStream tokenStream = new CommonTokenStream(lexer);
+            final bjmParser parser = new bjmParser(tokenStream);
+            return parser.program();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
